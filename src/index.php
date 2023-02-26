edbf168cbd0b2dd53d0d66d079e9fb6560a981f0
Dzmitry Paulau <dmitriy.pavlov.17.04@gmail.com>
2023-02-26 21:55:31 +0300

    refactor: change header sturcture, update styles

--- src/index.php
+++ src/index.php
@@ -7,63 +7,61 @@
   <title>Nîmes</title>
   <link rel="stylesheet" href="./style.css">
 </head>
-<body>
+<body class="home">
   <header class="header">
-    <div class="header__brand">
-      <h1 class="header__brand-text">Nîmes</h1>
-    </div>
-    <div class="header__wrapper">
-      <div class="container">
-        <div class="header__inner">
-          <nav class="header__nav-page nav-page">
-            <ul class="nav-page__list">
-              <li class="nav-page__item">
-                <a class="nav-page__link" href="#">Woman</a>
-              </li>
-              <li class="nav-page__item">
-                <a class="nav-page__link" href="#">Man</a>
-              </li>
-              <li class="nav-page__item">
-                <a class="nav-page__link" href="#">New</a>
-              </li>
-            </ul>
-          </nav>
-          <div class="header__collection">
-            <p class="header__slogan">The quality you deserve</p>
-            <p class="header__collection-name">Denim</p>
-            <a class="header__collection-link button" href="#">Check new collection</a>
-          </div>
-          <div class="header__image-wrapper">
-            <img class="header__image" src="./assets/images/6bd5cc90818e964627daeb381da06d08.jpeg" width="430" height="517" alt="close">
-          </div>
-          <nav class="header__nav-user nav-user">
-            <ul class="nav-user__list">
-              <li class="nav-user__item">
-                <a class="nav-user__link nav-user__link--cart" href="#">
-                  <span class="visually-hidden">Cart</span>
-                </a>
-              </li>
-              <li class="nav-user__item">
-                <a class="nav-user__link nav-user__link--search" href="#">
-                  <span class="visually-hidden">Search</span>
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <ul class="header__social-list social-list">
-            <li class="social-list__item">
-              <a class="social-list__link" href="#">Facebook</a>
-            </li>
-            <li class="social-list__item">
-              <a class="social-list__link" href="#">Instagram</a>
-            </li>
-          </ul>
-          <a class="header__skip-to-content" href="#">
-            <span class="visually-hidden">Skip to content</span>
+    <nav class="header__nav-page nav-page">
+      <ul class="nav-page__list">
+        <li class="nav-page__item">
+          <a class="nav-page__link" href="#">Woman</a>
+        </li>
+        <li class="nav-page__item">
+          <a class="nav-page__link" href="#">Man</a>
+        </li>
+        <li class="nav-page__item">
+          <a class="nav-page__link" href="#">New</a>
+        </li>
+      </ul>
+    </nav>
+    <h1 class="header__logo">Nîmes</h1>
+    <nav class="header__nav-user nav-user">
+      <ul class="nav-user__list">
+        <li class="nav-user__item">
+          <a class="nav-user__link nav-user__link--cart" href="#">
+            <span class="visually-hidden">Cart</span>
           </a>
-        </div>
-      </div>
-    </div>
+        </li>
+        <li class="nav-user__item">
+          <a class="nav-user__link nav-user__link--search" href="#">
+            <span class="visually-hidden">Search</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
   </header>
+  <main class="home__main">
+    <section class="home__promo promo">
+      <div class="promo__wrapper">
+        <div class="promo__collection">
+          <p class="promo__slogan">The quality you deserve</p>
+          <p class="promo__collection-name">Denim</p>
+          <a class="promo__collection-link button" href="#">Check new collection</a>
+        </div>
+        <div class="promo__image-wrapper">
+          <img class="promo__image" src="./assets/images/6bd5cc90818e964627daeb381da06d08.jpeg" width="430" height="517" alt="close">
+        </div>
+        <ul class="promo__social-list social-list">
+          <li class="social-list__item">
+            <a class="social-list__link" href="#">Facebook</a>
+          </li>
+          <li class="social-list__item">
+            <a class="social-list__link" href="#">Instagram</a>
+          </li>
+        </ul>
+        <a class="promo__skip-to-content" href="#">
+          <span class="visually-hidden">Skip to content</span>
+        </a>
+      </div>
+    </section>
+  </main>
 </body>
 </html>
